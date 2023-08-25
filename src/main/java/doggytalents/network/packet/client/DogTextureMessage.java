2a93c3de2c85562623ed5786db078bff2ba7b744
QUENTIN453 <quentinlamauve@gmail.com>
2023-08-25 18:11:00 +0200

    prevent textures errors from dog index

--- src/main/java/doggytalents/network/packet/client/DogTextureMessage.java
+++ src/main/java/doggytalents/network/packet/client/DogTextureMessage.java
@@ -10,6 +10,8 @@ import doggytalents.network.AbstractMessage.AbstractServerMessage;
 
 public class DogTextureMessage extends AbstractServerMessage {
 
+    public static final int MAX_TEXTURE = 12;// don't forget to change this line to an another number if adding new
+                                             // textures for the dog # dangerzone # todo
     public int entityId, doggyTexture;
 
     public DogTextureMessage() {}
@@ -33,11 +35,15 @@ public class DogTextureMessage extends AbstractServerMessage {
 
     @Override
     public void process(EntityPlayer player, Side side) {
+        if (doggyTexture < 0 || doggyTexture >= MAX_TEXTURE) {
+            return;
+        }
         Entity target = player.worldObj.getEntityByID(this.entityId);
         if (!(target instanceof EntityDog)) return;
 
         EntityDog dog = (EntityDog) target;
 
         dog.setTameSkin(this.doggyTexture);
+
     }
 }
